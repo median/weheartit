cd1a673c83f50c63e4e3bbf6dbe5b515d0310e44
yani <53308945+luxyren@users.noreply.github.com>
2021-10-01 14:52:51 +0100

    Add files via upload

---
+++ weheartit.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def look_up(user):
+    res = requests.get(f'https://weheartit.com/{str(user)}').text
+    soup = BeautifulSoup(res, "html.parser")
+    tags = soup.find_all('h1', class_='h1 no-margin text-overflow')
+
+    results = []
+    for result in tags:
+        results.append(result)
+    
+    if not results:
+        print(f'The username {user} is available!')
+
+with open('list.txt', 'r') as file:
+    for user in file:
+        look_up(user.strip())
